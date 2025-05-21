5f2d3c4a197ccaf7afb665aa200fad880417036e
wacamolis <carolasturias22@gmail.com>
2025-05-21 09:14:56 -0600

    se agrego html a hola mundo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo");
+Código
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hola Mundo</title>
+</head>
+<body>
+  <h1>Mi primera página</h1>
+  <script>
+    alert("Hola mundo"); // Muestra el cuadro de diálogo
+    console.log("Hola mundo"); // Muestra en la consola
+  </script>
+</body>
+</html>
